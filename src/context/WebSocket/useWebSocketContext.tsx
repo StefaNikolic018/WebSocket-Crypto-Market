e9adfde9de05b040eb751ed5865b04e778f3d5c2
Stefan Nikolic <stefanikolic018@gmail.com>
2023-03-22 12:03:08 +0100

    refactor: :fire: removed websocket context
    created hook for websocket and removed context of it

--- src/context/WebSocket/useWebSocketContext.tsx
+++
@@ -1,9 +0,0 @@
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-import React, { useContext } from 'react'
-import { WebSocketContext } from './WebSocketContextProvider'
-
-export default function useWebSocketContext() {
-  // data = {isReady: boolean, pairs: PairsObject}
-  const data = useContext(WebSocketContext)
-  return data
-}
