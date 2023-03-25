887a483ac69c5b1dbc3126bc2f667ae19f9cbe63
Stefan Nikolic <stefanikolic018@gmail.com>
2023-03-25 22:06:48 +0100

    refactor: :art: moved table loader to it's own component

--- src/components/Dashboard/Body.tsx
+++ src/components/Dashboard/Body.tsx
@@ -1,25 +1,14 @@
 /* eslint-disable @typescript-eslint/no-non-null-assertion */
 /* eslint-disable no-extra-boolean-cast */
 import React, { memo } from 'react'
-import Loader from '../Loader'
 
 import PairI from '../../interfaces/pairs'
+import TableLoader from './TableLoader'
 
 const Body = ({ pairs }: { pairs: PairI[] }) => {
   return (
     <tbody className="bg-white text-sm font-light text-gray-600 shadow-md ">
-      {!!pairs.length ? (
-        pairs
-      ) : (
-        <tr>
-          <td
-            className="animate-pulse py-3 text-center text-xl font-bold"
-            colSpan={6}
-          >
-            <Loader />
-          </td>
-        </tr>
-      )}
+      {!!pairs.length ? pairs : <TableLoader />}
     </tbody>
   )
 }
