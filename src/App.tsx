4f029d5c517db8ecb09da5d8f427d538f1046e2b
Stefan Nikolic <stefanikolic018@gmail.com>
2023-03-31 10:37:52 +0200

    refactor: :art: moved user context provider and browser router to app component from main

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,18 @@
 import React, { ReactElement } from 'react'
 import Router from './components/Router'
 import Navigation from './components/Navigation'
+import { BrowserRouter } from 'react-router-dom'
+import UserContextProvider from './context/User/UserContext'
 
 function App(): ReactElement {
   return (
     <div className="flex h-screen w-screen flex-col">
-      <Navigation />
-      <Router />
+      <BrowserRouter>
+        <UserContextProvider>
+          <Navigation />
+          <Router />
+        </UserContextProvider>
+      </BrowserRouter>
     </div>
   )
 }
--- src/main.tsx
+++ src/main.tsx
@@ -2,15 +2,7 @@
 import { createRoot } from 'react-dom/client'
 import './App.css'
 import App from './App'
-import { BrowserRouter } from 'react-router-dom'
-import UserContextProvider from './context/User/UserContext'
 
 const container = document.getElementById('root')
 const root = createRoot(container!)
-root.render(
-  <BrowserRouter>
-    <UserContextProvider>
-      <App />
-    </UserContextProvider>
-  </BrowserRouter>
-)
+root.render(<App />)
